c42fd671a03fc44ed1dcb61b0d5abc9154059176
Emperores <1443957775@qq.com>
2015-11-18 11:40:47 +0800

    liguangxings

--- Love_PartTimeJob_app/app/src/main/java/com/best/love_parttimejob_app/LoginActivity.java
+++ Love_PartTimeJob_app/app/src/main/java/com/best/love_parttimejob_app/LoginActivity.java
@@ -2,6 +2,7 @@ package com.best.love_parttimejob_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -31,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.i("asd","");
             return true;
         }
 
