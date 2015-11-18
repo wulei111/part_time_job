b5ce6b725ce9a2c17236c1317df3bf5346719168
Emperores <1443957775@qq.com>
2015-11-18 14:33:35 +0800

    add ValidateLoginActivity 、xml

--- Love_PartTimeJob_app/app/src/main/java/com/best/love_parttimejob_app/LoginActivity.java
+++ Love_PartTimeJob_app/app/src/main/java/com/best/love_parttimejob_app/LoginActivity.java
@@ -2,38 +2,25 @@ package com.best.love_parttimejob_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
 
-import com.best.love_parttimejob_app.R;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    public TextView duanxin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        duanxin = (TextView) findViewById(R.id.duanxin);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+//    短信验证登录
+    public void duanxinlogin(View view){
+        Intent intent = new Intent(this,ValidateLoginActivity.class);
+        startActivity(intent);
     }
 }
